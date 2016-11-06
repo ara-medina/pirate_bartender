3b113792093e616d70da7896a065147159063d11
Sandra Medina Corey <s.medinacorey@gmail.com>
2016-11-06 13:50:57 -0500

    asks the user a series of questions to determine their taste

---
+++ main.py
@@ -0,0 +1,27 @@
+questions = {
+    "strong": "Do ye like yer drinks strong?",
+    "salty": "Do ye like it with a salty tang?",
+    "bitter": "Are ye a lubber who likes it bitter?",
+    "sweet": "Would ye like a bit of sweetness with yer poison?",
+    "fruity": "Are ye one for a fruity finish?",
+}
+
+ingredients = {
+    "strong": ["glug of rum", "slug of whisky", "splash of gin"],
+    "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
+    "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
+    "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
+    "fruity": ["slice of orange", "dash of cassis", "cherry on top"],
+}
+
+user_responses = {}
+
+for key,question in questions.items():
+    user_input = input(question)
+    if (user_input == 'y') or (user_input == 'yes'): 
+        user_responses[key] = True
+    else:
+        user_responses[key] = False
+
+print(user_responses)
+
